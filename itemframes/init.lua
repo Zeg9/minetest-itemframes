3e48843b09f0a83bf455468c818a8db9bdd74a08
Zeg9 <dazeg9@gmail.com>
2013-04-15 17:44:27 +0200

    Copy selection box for older minetest versions

--- itemframes/init.lua
+++ itemframes/init.lua
@@ -53,6 +53,7 @@ minetest.register_node("itemframes:frame",{
 	description = "Item frame",
 	drawtype = "nodebox",
 	node_box = { type = "fixed", fixed = {-0.5, -0.5, 7/16, 0.5, 0.5, 0.5} },
+	selection_box = { type = "fixed", fixed = {-0.5, -0.5, 7/16, 0.5, 0.5, 0.5} },
 	tiles = {"itemframes_frame.png"},
 	inventory_image = "itemframes_frame.png",
 	wield_image = "itemframes_frame.png",
